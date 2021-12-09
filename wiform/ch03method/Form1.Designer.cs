0e230aa5c4345d7800659e09ed7890a707464b6c
Kwang Myung Yu <km.yu99@samsung.com>
2021-12-09 15:13:26 +0900

    Add winform ch03method

---
+++ wiform/ch03method/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ch03method
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tboxResult = new System.Windows.Forms.TextBox();
+            this.tboxNum1 = new System.Windows.Forms.TextBox();
+            this.tboxNum2 = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tboxResult
+            // 
+            this.tboxResult.Enabled = false;
+            this.tboxResult.Location = new System.Drawing.Point(13, 13);
+            this.tboxResult.Name = "tboxResult";
+            this.tboxResult.Size = new System.Drawing.Size(284, 20);
+            this.tboxResult.TabIndex = 0;
+            this.tboxResult.Text = "0";
+            this.tboxResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tboxNum1
+            // 
+            this.tboxNum1.Location = new System.Drawing.Point(13, 39);
+            this.tboxNum1.Name = "tboxNum1";
+            this.tboxNum1.Size = new System.Drawing.Size(139, 20);
+            this.tboxNum1.TabIndex = 0;
+            this.tboxNum1.Text = "0";
+            this.tboxNum1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tboxNum2
+            // 
+            this.tboxNum2.Location = new System.Drawing.Point(158, 39);
+            this.tboxNum2.Name = "tboxNum2";
+            this.tboxNum2.Size = new System.Drawing.Size(139, 20);
+            this.tboxNum2.TabIndex = 0;
+            this.tboxNum2.Text = "0";
+            this.tboxNum2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlus.Location = new System.Drawing.Point(13, 77);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(62, 43);
+            this.btnPlus.TabIndex = 1;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus.Location = new System.Drawing.Point(90, 77);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(62, 43);
+            this.btnMinus.TabIndex = 1;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMulti.Location = new System.Drawing.Point(158, 77);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(62, 43);
+            this.btnMulti.TabIndex = 1;
+            this.btnMulti.Text = "*";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivision.Location = new System.Drawing.Point(235, 77);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(62, 43);
+            this.btnDivision.TabIndex = 1;
+            this.btnDivision.Text = "/";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 132);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.tboxNum2);
+            this.Controls.Add(this.tboxNum1);
+            this.Controls.Add(this.tboxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tboxResult;
+        private System.Windows.Forms.TextBox tboxNum1;
+        private System.Windows.Forms.TextBox tboxNum2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnDivision;
+    }
+}
+
---
+++ wiform/ch03method/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch03method
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            int iNumA =0,  iNumB = 0;
+            if (int.TryParse(tboxNum1.Text.ToString(), out iNumA) && int.TryParse(tboxNum2.Text.ToString(), out iNumB))
+            {
+                tboxResult.Text = fPlus(iNumA, iNumB).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please check input!");
+            }
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            int iNumA = 0, iNumB = 0;
+            if (int.TryParse(tboxNum1.Text.ToString(), out iNumA) && int.TryParse(tboxNum2.Text.ToString(), out iNumB))
+            {
+                tboxResult.Text = fMinus(iNumA, iNumB).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please check input!");
+            }
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            int iNumA = 0, iNumB = 0;
+            if (int.TryParse(tboxNum1.Text.ToString(), out iNumA) && int.TryParse(tboxNum2.Text.ToString(), out iNumB))
+            {
+                tboxResult.Text = fMulti(iNumA, iNumB).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please check input!");
+            }
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            int iNumA = 0, iNumB = 0;
+            if (int.TryParse(tboxNum1.Text.ToString(), out iNumA) && int.TryParse(tboxNum2.Text.ToString(), out iNumB))
+            {
+                tboxResult.Text = fDivide(iNumA, iNumB).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please check input!");
+            }
+        }
+
+        private int fPlus(int iA, int iB)
+        {
+            return iA + iB;
+        }
+
+        private int fMinus(int iA, int iB)
+        {
+            return iA - iB;
+        }
+
+        private int fMulti(int iA, int iB)
+        {
+            return iA * iB;
+        }
+
+        private int fDivide(int iA, int iB)
+        {
+            return iA / iB;
+        }
+    }
+}

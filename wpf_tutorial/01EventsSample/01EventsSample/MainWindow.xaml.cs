4542e75ac022b1fa8a4e6e77d9b900a3439de134
Kwang Myung Yu <km.yu99@samsung.com>
2022-02-11 17:34:05 +0900

    Add tutorials

---
+++ wpf_tutorial/01EventsSample/01EventsSample/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿//https://wpf-tutorial.com/xaml/events-in-xaml/
+// 이벤트 추가 여러가지...
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01EventsSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            pnlMainGrid.MouseUp += new MouseButtonEventHandler(pnlMainGrid_MouseUp);
+        }
+
+
+
+        private void pnlMainGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("You clicked me at" + e.GetPosition(this).ToString());
+        }
+
+
+    }
+}
